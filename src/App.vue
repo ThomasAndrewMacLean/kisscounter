d6a1a9f6be6a69cd0da78562bf807b850a90540e
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-12-18 19:16:03 +0100

    first init 😘

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+// src/App.vue
+
+<template>
+  <div class="container">
+    <h1>{{ message }}</h1>
+    <span class="kiss" v-for="(kiss,index) in kisses" v-bind:key="index">
+      <div class="kiss1" @click="setKissedToTrue(index)">{{kiss}}</div>
+    </span>
+
+    <br>
+    <button @click="resetKisses">Reset</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      message: "Kiss Counter",
+      kisses: Array(102).fill("😘")
+    };
+  },
+  methods: {
+    resetKisses() {
+      this.kisses = Array(102).fill("😘");
+    },
+    setKissedToTrue(index) {
+      this.$set(this.kisses, index, "❌");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  width: 100vw;
+  margin: 50px auto;
+  text-align: center;
+}
+
+.kiss {
+  font-size: 5rem;
+  margin: 1rem;
+  cursor: pointer;
+}
+.kiss1 {
+  display: inline-flex;
+}
+</style>
